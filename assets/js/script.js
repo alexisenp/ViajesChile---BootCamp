7da7413c065038fabe1fcb7a9d2ad38b0e3ea004
Alexisenp <alexisenp@gmail.com>
2024-07-17 22:04:32 -0400

    Se agregan comentarios al codigo y fondo al menu

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,3 +23,15 @@ if (toastTrigger) {
     toastBootstrap.show()
   })
 }
+
+
+// Añadir evento de scroll para cambiar el fondo de la barra de navegación
+window.addEventListener('scroll', function() {
+    const navbar = document.querySelector('.navbar');
+    if (window.scrollY > 50) { // Cambia este valor según sea necesario
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
+
